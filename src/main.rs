16f8959a9191d9aba38f95099fe5a18ec160a047
Kim Desrosiers <kimdesro@gmail.com>
2018-03-04 21:08:16 -0500

    Trying to implement a http endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,34 @@
-extern crate x264;
-extern crate rscam;
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
 
-use rscam::{Camera, Config};
+extern crate rocket;
+extern crate baby_watcher;
+
+use baby_watcher::start_stream;
+use std::thread;
+use std::io;
 use std::fs::File;
 use std::io::prelude::*;
-use x264::{Encoding, Image, Setup, Preset, Tune};
+
+#[get("/launch_stream")]
+fn launch_stream() -> &'static str {
+    thread::spawn(|| {
+        start_stream();
+    });
+    "Stream started"
+}
+
+#[get("/stream")]
+fn get_stream(){
+}
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to baby watcher"
+}
+
 
 fn main() {
-    let mut camera = Camera::new("/dev/video0").unwrap();
-
-    camera.start(&Config {
-        interval: (1, 30),
-        resolution: (1280, 720),
-        format: b"RGB3",
-        ..Default::default()
-    }).unwrap();
-
-
-    let mut encoder =
-        Setup::preset(Preset::Ultrafast, Tune::None, false, false)
-            .fps(30,1)
-            .build(Encoding::RGB, 1280, 720)
-            .unwrap();
-    let mut file = File::create("fade.h264").unwrap();
-    let mut canvas = vec![0; 1280 * 720 * 3];
-
-    println!("Initialized!");
-
-    {
-        let headers = encoder.headers().unwrap();
-        file.write_all(headers.entirety()).unwrap();
-    }
-
-    for i in 0..300 {
-        let frame = camera.capture().unwrap();
-        let image =  Image::rgb(1280 as i32, 720 as i32, &frame);
-        let (data, _) = encoder.encode(i as i64, image).unwrap();
-        file.write_all(data.entirety()).unwrap();
-    }
-
-    {
-        let mut flush = encoder.flush();
-        while let Some(result) = flush.next() {
-            let (data, _) = result.unwrap();
-            file.write_all(data.entirety()).unwrap();
-        }
-    }
-
-    println!("Done! The output is at `fade.h264`.");
-    println!("Try playing it with VLC, and prepare to be underwhelmed! ;)");
+    rocket::ignite().mount("/", routes![index, launch_stream, get_stream]).launch();
 }
